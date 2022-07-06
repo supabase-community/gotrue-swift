d993e8861d87cca57337dd241050d8d3409b8d65
Guilherme Souza <grsouza@pm.me>
2022-07-06 06:23:53 -0300

    Add missing attributes to models

--- Sources/GoTrue/AuthChangeEvent.swift
+++ Sources/GoTrue/AuthChangeEvent.swift
@@ -1,7 +1,8 @@
 public enum AuthChangeEvent: String {
+  case passwordRecovery = "PASSWORD_RECOVERY"
   case signedIn = "SIGNED_IN"
   case signedOut = "SIGNED_OUT"
+  case tokenRefreshed = "TOKEN_REFRESHED"
   case userUpdated = "USER_UPDATED"
-  //  case userDeleted = "USER_DELETED"
-  case passwordRecovery = "PASSWORD_RECOVERY"
+  case userDeleted = "USER_DELETED"
 }
