1158ded23fe9d1ac98406afd96d6ef3d5ba2ace0
Guilherme Souza <grsouza@pm.me>
2022-07-27 06:35:32 -0300

    Update Examples project

--- Examples/Shared/Secrets.swift
+++ Examples/Shared/Secrets.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 
-let SUPABASE_URL = URL(string: "https://{PROJECT_ID}.supabase.co/auth/v1")!
-let SUPABASE_API_KEY = "{PROJECT_API_KEY}"
+let SUPABASE_URL = URL(string: "https://PROJECT_ID.supabase.co/auth/v1")!
+let SUPABASE_API_KEY = "PROJECT_API_KEY"
