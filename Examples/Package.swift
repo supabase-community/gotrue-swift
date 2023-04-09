3490510cb2e5837cd1d76d70c7a913ae9b8c299b
Danh Nguyen <danhdevelop@gmail.com>
2023-04-09 20:24:02 +0700

    Add Google Sign In sample code

--- Examples/Package.swift
+++ Examples/Package.swift
@@ -2,10 +2,25 @@
 
 import PackageDescription
 
+// let package = Package(
+//   name: "Examples",
+//   platforms: [],
+//   products: [],
+//   dependencies: [],
+//   targets: []
+// )
+
+let packageName = "Shared" // <-- Change this to yours
 let package = Package(
-  name: "Examples",
-  platforms: [],
-  products: [],
-  dependencies: [],
-  targets: []
+  name: "",
+  // platforms: [.iOS("9.0")],
+  products: [
+    .library(name: packageName, targets: [packageName])
+  ],
+  targets: [
+    .target(
+      name: packageName,
+      path: packageName
+    )
+  ]
 )
--- Examples/Shared/Sources/AppView.swift
+++ Examples/Shared/Sources/AppView.swift
@@ -1,23 +1,76 @@
 import GoTrue
 import SwiftUI
+import AuthenticationServices
+import Combine
+
+class SignInViewModel: NSObject, ObservableObject, ASWebAuthenticationPresentationContextProviding {
+    
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return ASPresentationAnchor()
+    }
+    
+    func signInGoogle(client: GoTrueClient) {
+        
+        /* Config callback url from Google Cloud Console with following guide
+         https://supabase.com/docs/guides/auth/social-login/auth-google#find-your-callback-url
+         */
+        
+        // Need to config Url Type in Info.plist match with below Scheme
+        let schemeUrl = "supabase"
+        
+        do {
+            let url = try client.getOAuthSignInURL(
+                provider: Provider.google
+                
+                // Need to config scheme url in Supabase Auth console panel
+                ,redirectTo: URL(string: "\(schemeUrl)://auth")
+            )
+            let handler: ASWebAuthenticationSession.CompletionHandler = { (url, error) in
+                if let error = error {
+                    NSLog("Error \(error)")
+                } else if let url = url {
+                    Task {
+                        try await client.session(from: url)
+                    }
+                }
+            }
+            let session = ASWebAuthenticationSession(
+                url: url,
+                callbackURLScheme: schemeUrl,
+                completionHandler: handler
+            )
+            session.presentationContextProvider = self
+            session.prefersEphemeralWebBrowserSession = true
+            session.start()
+        } catch {
+            NSLog("Error \(error)")
+        }
+    }
+}
 
 struct AppView: View {
-  @Environment(\.goTrueClient) private var client
-  @State private var session: Session?
-  @State private var clientInitialized = false
-
+    @Environment(\.goTrueClient) private var client
+    @State private var session: Session?
+    @State private var clientInitialized = false
+    @StateObject var signInModel = SignInViewModel()
+    
   var body: some View {
     if clientInitialized {
       NavigationView {
         if let session {
           SessionView(session: session)
         } else {
-          List {
-            NavigationLink("Auth with Email and Password") {
-              AuthWithEmailAndPasswordView()
+            Section {
+                List {
+                  NavigationLink("Auth with Email and Password") {
+                      AuthWithEmailAndPasswordView()
+                  }
+                    Button("Google Sign In") {
+                        signInModel.signInGoogle(client: client)
+                    }
+                }
+                .listStyle(.plain)
             }
-          }
-          .listStyle(.plain)
           .navigationTitle("Examples")
         }
       }
@@ -36,6 +89,7 @@ struct AppView: View {
       session = try? await client.session
     }
   }
+    
 }
 
 func stringfy<T: Codable>(_ value: T) -> String {
