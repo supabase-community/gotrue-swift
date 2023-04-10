7841fed4f91adc86628236603d6fad52edabf074
Guilherme Souza <grsouza@pm.me>
2023-04-10 06:30:30 -0300

    Add test

--- Tests/GoTrueTests/GoTrueTests.swift
+++ Tests/GoTrueTests/GoTrueTests.swift
@@ -40,7 +40,7 @@ final class GoTrueTests: XCTestCase {
   func testDecodeSessionOrUser() {
     XCTAssertNoThrow(
       try JSONDecoder.goTrue.decode(
-        AuthResponse.self, from: sessionJSON.data(using: .utf8)!
+        AuthResponse.self, from: json(named: "session")
       )
     )
   }
@@ -55,6 +55,19 @@ final class GoTrueTests: XCTestCase {
       XCTAssertEqual(user?.email, "guilherme@grds.dev")
     }
   #endif
+
+  func testSignInWithIdToken() async throws {
+    Mock(
+      url: URL(string: "http://localhost:54321/auth/v1/token?grant_type=id_token")!,
+      dataType: .json,
+      statusCode: 200,
+      data: [.post: json(named: "session")]
+    ).register()
+
+    let session = try await sut
+      .signInWithIdToken(credentials: OpenIDConnectCredentials(token: "dummy-token-1234"))
+    XCTAssertEqual(session.user.email, "guilherme@binaryscraping.co")
+  }
 
   func testSignInWithProvider() throws {
     let url = try sut.getOAuthSignInURL(
@@ -110,43 +123,3 @@ final class GoTrueTests: XCTestCase {
     }
   }
 }
-
-let sessionJSON = """
-{
-"access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjQ4NjQwMDIxLCJzdWIiOiJmMzNkM2VjOS1hMmVlLTQ3YzQtODBlMS01YmQ5MTlmM2Q4YjgiLCJlbWFpbCI6Imd1aWxoZXJtZTJAZ3Jkcy5kZXYiLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQifQ.4lMvmz2pJkWu1hMsBgXP98Fwz4rbvFYl4VA9joRv6kY",
-"token_type": "bearer",
-"expires_in": 3600,
-"refresh_token": "GGduTeu95GraIXQ56jppkw",
-"user": {
-"id": "f33d3ec9-a2ee-47c4-80e1-5bd919f3d8b8",
-"aud": "authenticated",
-"role": "authenticated",
-"email": "guilherme2@grds.dev",
-"email_confirmed_at": "2022-03-30T10:33:41.018575157Z",
-"phone": "",
-"last_sign_in_at": "2022-03-30T10:33:41.021531328Z",
-"app_metadata": {
-"provider": "email",
-"providers": [
-"email"
-]
-},
-"user_metadata": {},
-"identities": [
-{
-"id": "f33d3ec9-a2ee-47c4-80e1-5bd919f3d8b8",
-"user_id": "f33d3ec9-a2ee-47c4-80e1-5bd919f3d8b8",
-"identity_data": {
-  "sub": "f33d3ec9-a2ee-47c4-80e1-5bd919f3d8b8"
-},
-"provider": "email",
-"last_sign_in_at": "2022-03-30T10:33:41.015557063Z",
-"created_at": "2022-03-30T10:33:41.015612Z",
-"updated_at": "2022-03-30T10:33:41.015616Z"
-}
-],
-"created_at": "2022-03-30T10:33:41.005433Z",
-"updated_at": "2022-03-30T10:33:41.022688Z"
-}
-}
-"""
